33b0cc4d66c3823cee495774b2b33e500992cb3f
Marina Sirenko <sirenko.workspace@gmail.com>
2026-04-10 10:22:27 +0700

    bug: fix issue after Lighthouse check

--- frontend/src/shared/ui/layout/IllustrationLayout.vue
+++ frontend/src/shared/ui/layout/IllustrationLayout.vue
@@ -4,11 +4,11 @@
 
 <template>
   <div class="grid min-h-dvh w-full min-w-0 grid-cols-1 lg:grid-cols-[560px_minmax(0,1fr)]">
-    <div
+    <main
       class="flex min-h-dvh min-h-0 flex-col bg-surface max-[450px]:px-4"
     >
       <slot />
-    </div>
+    </main>
 
     <aside class="relative hidden min-h-dvh min-h-0 flex-col items-center justify-center overflow-hidden bg-gradient-to-br from-[#f4f7fb] to-[#e4eef8] lg:flex"
     >
